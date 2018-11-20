cd1d9f00f287f52f3a1e82e635a77d1ffecf2e9c
Andrea Censi <acensi@ethz.ch>
2018-11-20 21:19:55 +0100

    fix

--- testing-cookies/go.sh
+++ testing-cookies/go.sh
@@ -1,2 +1,2 @@
-
-cookiecutter .. project_name="Foobar library" project_slug=foobar
+set -e
+cookiecutter -c master18 --no-input git@github.com:duckietown/duckietown-project-template.git  project_name="Foobar library" project_slug=foobar
