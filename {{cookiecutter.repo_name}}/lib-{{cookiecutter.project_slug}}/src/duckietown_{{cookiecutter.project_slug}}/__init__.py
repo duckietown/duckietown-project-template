3c2c0bbb00084cde3ad6f3177cf9e3133e15ceae
Andrea Censi <acensi@ethz.ch>
2021-06-16 16:45:17 +0200

    formatting

--- {{cookiecutter.repo_name}}/lib-{{cookiecutter.project_slug}}/src/duckietown_{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.repo_name}}/lib-{{cookiecutter.project_slug}}/src/duckietown_{{cookiecutter.project_slug}}/__init__.py
@@ -1,13 +1,13 @@
-# coding=utf-8 
-__version__ = '{{cookiecutter.version}}'
+# coding=utf-8
+__version__ = "{{cookiecutter.version}}"
 
 import logging
 
 logging.basicConfig()
-logger = logging.getLogger('dt-{{cookiecutter.project_slug}}')
+logger = logging.getLogger("dt-{{cookiecutter.project_slug}}")
 logger.setLevel(logging.DEBUG)
 
-logger.info('{{cookiecutter.python_package}} %s' % __version__)
+logger.info("{{cookiecutter.python_package}} %s" % __version__)
 
 from .algo import *
 
